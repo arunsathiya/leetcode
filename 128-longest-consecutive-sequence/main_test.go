363effaf84916ce90d35a2776a2009cfa410f89d
Arun <arun@arun.blog>
2023-12-18 15:11:31 -0800

    Setup test cases
    Signed-off-by: Arun <arun@arun.blog>

--- 128-longest-consecutive-sequence/main_test.go
+++ 128-longest-consecutive-sequence/main_test.go
@@ -2,5 +2,29 @@ package main
 
 import "testing"
 
-func TestMain(t *testing.T) {}
-
+func TestLongestConsecutive(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  []int
+		output int
+	}{
+		{
+			name:   "test case 2",
+			input:  []int{0, 3, 7, 2, 5, 8, 4, 6, 0, 1},
+			output: 9,
+		},
+		{
+			name:   "test case 1",
+			input:  []int{100, 4, 200, 1, 3, 2},
+			output: 4,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			output := longestConsecutive(test.input)
+			if output != test.output {
+				t.Errorf("Expected %d, got %d", test.output, output)
+			}
+		})
+	}
+}
