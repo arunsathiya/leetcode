64bff16bd94907f46fc988f562e6d7e07f10dc1b
Arun <arun@arun.blog>
2023-12-18 15:12:31 -0800

    Sort numbers in the input
    Signed-off-by: Arun <arun@arun.blog>

--- 128-longest-consecutive-sequence/main.go
+++ 128-longest-consecutive-sequence/main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+func sort(nums []int) []int {
+	n := len(nums)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if nums[j] > nums[j+1] {
+				nums[j], nums[j+1] = nums[j+1], nums[j]
+			}
+		}
+	}
+	return nums
+}
+
+func longestConsecutive(nums []int) int {}
