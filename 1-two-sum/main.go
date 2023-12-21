6a096f082c9cdab16569da398205d66a7672e008
Arun <arun@arun.blog>
2023-12-21 13:42:03 -0800

    Solve two sum using pointers approach
    Signed-off-by: Arun <arun@arun.blog>

---
+++ 1-two-sum/main.go
@@ -0,0 +1,17 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+	left := 0
+	right := len(nums) - 1
+	for left < right {
+		sum := nums[left] + nums[right]
+		if sum > target {
+			right--
+		} else if sum < target {
+			left++
+		} else {
+			return []int{left, right}
+		}
+	}
+	return nil
+}
