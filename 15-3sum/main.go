355304329ecbb416f19865384eea79d78628364f
Arun <arun@arun.blog>
2023-12-21 13:23:40 -0800

    Implement learnings from neetcode and tests pass
    Signed-off-by: Arun <arun@arun.blog>

--- 15-3sum/main.go
+++ 15-3sum/main.go
@@ -9,18 +9,25 @@ func (a Intslice) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a Intslice) Less(i, j int) bool { return a[i] < a[j] }
 
 func threeSum(nums []int) [][]int {
-	sort.Sort(Intslice(nums))
 	output := make([][]int, 0)
-	for i := range nums {
-		left := i + 1
-		right := len(nums) - 1
+	sort.Sort(Intslice(nums))
+	for i, num := range nums {
+		if i > 0 && num == nums[i-1] {
+			continue
+		}
+		left, right := i+1, len(nums)-1
 		for left < right {
-			if nums[i]+nums[left]+nums[right] == 0 {
-				output = append(output, []int{nums[i], nums[left], nums[right]})
-			} else if nums[i]+nums[left]+nums[right] < 0 {
+			sum := num + nums[left] + nums[right]
+			if sum > 0 {
+				right--
+			} else if sum < 0 {
 				left++
 			} else {
-				right--
+				output = append(output, []int{num, nums[left], nums[right]})
+				left++
+				for nums[left] == nums[left-1] {
+					left++
+				}
 			}
 		}
 	}
