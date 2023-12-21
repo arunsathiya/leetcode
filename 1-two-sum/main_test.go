36465e13e772b579cb195e803d912519eb72b93a
Arun <arun@arun.blog>
2023-12-21 13:44:39 -0800

    Update with failing test case
    Signed-off-by: Arun <arun@arun.blog>

--- 1-two-sum/main_test.go
+++ 1-two-sum/main_test.go
@@ -18,6 +18,12 @@ func TestMain(t *testing.T) {
 			target:   9,
 			expected: []int{0, 1},
 		},
+		{
+			name:     "test case from leetcode",
+			input:    []int{3, 2, 4},
+			target:   6,
+			expected: []int{1, 2},
+		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
