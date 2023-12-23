925d09f7fcb17c12611055afa299d7e8211827ea
Arun <arun@arun.blog>
2023-12-23 12:23:04 -0800

    Binary search without initializing a results slice
    Signed-off-by: Arun <arun@arun.blog>

--- 153-find-minimum-in-rotated-sorted-array/main.go
+++ 153-find-minimum-in-rotated-sorted-array/main.go
@@ -1,20 +1,17 @@
 package main
 
 func findMin(nums []int) int {
-	result := nums[0]
 	left, right := 0, len(nums)-1
 	for left <= right {
 		if nums[left] < nums[right] {
-			result = min(result, nums[left])
-			break
+			return nums[left]
 		}
 		mid := (left + right) / 2
-		result = min(result, nums[mid])
 		if nums[mid] >= nums[left] {
 			left = mid + 1
 		} else {
 			right = mid - 1
 		}
 	}
-	return result
+	return nums[0]
 }
