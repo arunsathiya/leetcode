9068d309ec65f6f0422fb835a4c54fd255d00660
Arun <arun@arun.blog>
2023-12-22 22:42:52 -0800

    Prepare hashmap with count for each value
    Signed-off-by: Arun <arun@arun.blog>

--- 347-top-k-frequent-elements/main.go
+++ 347-top-k-frequent-elements/main.go
@@ -1,5 +1,13 @@
 package main
 
 func topKFrequent(nums []int, k int) []int {
+	hashmap := make(map[int]int, 0)
+	for _, num := range nums {
+		if _, ok := hashmap[num]; !ok {
+			hashmap[num] = 1
+		} else {
+			hashmap[num] += 1
+		}
+	}
 	return nil
 }
