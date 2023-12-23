7bbb7da3cb151ee7dc4a4a42ce694531a75c55ef
Arun <arun@arun.blog>
2023-12-22 23:14:02 -0800

    Sort nums based on descending order of hashmap's frequencies, and then loop through sorts nums to get the top k matches
    Signed-off-by: Arun <arun@arun.blog>

--- 347-top-k-frequent-elements/main.go
+++ 347-top-k-frequent-elements/main.go
@@ -4,7 +4,6 @@ import "sort"
 
 func topKFrequent(nums []int, k int) []int {
 	hashmap := make(map[int]int, 0)
-	occurances := make([]int, 0)
 	results := make([]int, 0)
 	for _, num := range nums {
 		if _, ok := hashmap[num]; !ok {
@@ -13,15 +12,15 @@ func topKFrequent(nums []int, k int) []int {
 			hashmap[num] += 1
 		}
 	}
-	for _, v := range hashmap {
-		occurances = append(occurances, v)
-	}
-	sort.Slice(occurances, func(i, j int) bool {
-		return occurances[i] > occurances[j]
+	sort.Slice(nums, func(i, j int) bool {
+		return hashmap[nums[i]] > hashmap[nums[j]]
 	})
-	for k, v := range hashmap {
-		if v == occurances[0] || v == occurances[1] {
-			results = append(results, k)
+	for _, num := range nums {
+		if len(results) == 0 || results[len(results)-1] != num {
+			results = append(results, num)
+		}
+		if len(results) == k {
+			break
 		}
 	}
 	return results
