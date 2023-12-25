9322afd0298592c59a845cd95e9c54841cdc4bea
Arun <arun@arun.blog>
2023-12-25 13:20:18 -0800

    Setup test cases from leetcode
    Signed-off-by: Arun <arun@arun.blog>

---
+++ 20-valid-parentheses/main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func TestIsValid(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected bool
+	}{
+		{
+			name:     "first test case from leetcode",
+			input:    "()",
+			expected: true,
+		},
+		{
+			name:     "second test case from leetcode",
+			input:    "()[]{}",
+			expected: true,
+		},
+		{
+			name:     "third test case from leetcode",
+			input:    "(]",
+			expected: false,
+		},
+	}
+	for _, test := range tests {
+		output := isValid(test.input)
+		if output != test.expected {
+			t.Errorf("Expected %v, got %v", test.expected, output)
+		}
+	}
+}
