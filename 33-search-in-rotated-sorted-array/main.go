c54f3defd9d596a7ebc5a3ac08799ac907604e6d
Arun <arun@arun.blog>
2023-12-25 00:33:40 -0800

    Find index; thanks neetcode for the lesson
    Signed-off-by: Arun <arun@arun.blog>

--- 33-search-in-rotated-sorted-array/main.go
+++ 33-search-in-rotated-sorted-array/main.go
@@ -2,18 +2,24 @@ package main
 
 func search(nums []int, target int) int {
 	left, right := 0, len(nums)-1
-	for left < right {
-		if nums[left] < nums[right] {
-			return left
-		}
+	for left <= right {
 		mid := left + (right-left)/2
-		if nums[mid] > nums[right] {
-			left = mid + 1
-		} else if nums[mid] < nums[right] {
-			right = mid
+		if target == nums[mid] {
+			return mid
+		}
+		if nums[left] <= nums[mid] {
+			if target > nums[mid] || target < nums[left] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
 		} else {
-			right--
+			if target < nums[mid] || target > nums[right] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
 		}
 	}
-	return left
+	return -1
 }
