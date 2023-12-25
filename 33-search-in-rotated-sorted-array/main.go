562bae638f911de1dfb36d1dd6aad39725490dbb
Arun <arun@arun.blog>
2023-12-24 23:49:08 -0800

    Implement binary search to find the index of the lowest number
    Signed-off-by: Arun <arun@arun.blog>

--- 33-search-in-rotated-sorted-array/main.go
+++ 33-search-in-rotated-sorted-array/main.go
@@ -1,5 +1,19 @@
 package main
 
 func search(nums []int, target int) int {
-	return 0
+	left, right := 0, len(nums)-1
+	for left < right {
+		if nums[left] < nums[right] {
+			return left
+		}
+		mid := left + (right-left)/2
+		if nums[mid] > nums[right] {
+			left = mid + 1
+		} else if nums[mid] < nums[right] {
+			right = mid
+		} else {
+			right--
+		}
+	}
+	return left
 }
