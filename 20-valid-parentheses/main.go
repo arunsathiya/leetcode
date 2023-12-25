7fe0f52b55fb569fedf45162d08c08e2a22a2064
Arun <arun@arun.blog>
2023-12-25 13:35:13 -0800

    Implement stack to use for the problem
    Signed-off-by: Arun <arun@arun.blog>

--- 20-valid-parentheses/main.go
+++ 20-valid-parentheses/main.go
@@ -1,5 +1,32 @@
 package main
 
+type Stack []string
+
+func (s *Stack) Push(str string) {
+	*s = append(*s, str)
+}
+
+func (s *Stack) Pop() string {
+	if len(*s) == 0 {
+		return ""
+	}
+	index := len(*s) - 1
+	element := (*s)[index]
+	*s = (*s)[:index]
+	return element
+}
+
+func (s *Stack) Peek() string {
+	if len(*s) == 0 {
+		return ""
+	}
+	return (*s)[len(*s)-1]
+}
+
+func (s *Stack) isEmpty() bool {
+	return len(*s) == 0
+}
+
 func isValid(s string) bool {
 	for i := range s {
 		if string(s[i]) == "(" && string(s[len(s)-1]) == ")" {
