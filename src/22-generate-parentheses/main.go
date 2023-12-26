a2b181043c475c4b072007017779cc9b8923d072
Arun <arun@arun.blog>
2023-12-25 16:50:18 -0800

    Use backtracking to solve the problem, thanks neetcode
    Signed-off-by: Arun <arun@arun.blog>

--- src/22-generate-parentheses/main.go
+++ src/22-generate-parentheses/main.go
@@ -1,5 +1,20 @@
 package main
 
+func backtrack(result *[]string, path string, open, close, max int) {
+	if len(path) == max*2 {
+		*result = append(*result, path)
+		return
+	}
+	if open < max {
+		backtrack(result, path+"(", open+1, close, max)
+	}
+	if close < open {
+		backtrack(result, path+")", open, close+1, max)
+	}
+}
+
 func generateParenthesis(n int) []string {
-	return nil
+	var result []string
+	backtrack(&result, "", 0, 0, n)
+	return result
 }
