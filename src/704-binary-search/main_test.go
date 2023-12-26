1ca2f7ce2f5c68ca18b6cb6e69176265252573df
Arun <arun@arun.blog>
2023-12-25 17:01:44 -0800

    Setup test cases from leetcode
    Signed-off-by: Arun <arun@arun.blog>

--- src/704-binary-search/main_test.go
+++ src/704-binary-search/main_test.go
@@ -2,5 +2,32 @@ package main
 
 import "testing"
 
-func TestMain(t *testing.T) {}
-
+func TestSearch(t *testing.T) {
+	tests := []struct {
+		name     string
+		nums     []int
+		target   int
+		expected int
+	}{
+		{
+			name:     "first test case from leetcode",
+			nums:     []int{-1, 0, 3, 5, 9, 12},
+			target:   9,
+			expected: 4,
+		},
+		{
+			name:     "second test case from leetcode",
+			nums:     []int{-1, 0, 3, 5, 9, 12},
+			target:   2,
+			expected: -1,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			output := search(test.nums, test.target)
+			if output != test.expected {
+				t.Errorf("Expected %d, got %d", test.expected, output)
+			}
+		})
+	}
+}
