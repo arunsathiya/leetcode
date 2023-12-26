df19a56a3fa918c19acba6e94fe3f36f977bd578
Arun <arun@arun.blog>
2023-12-25 17:08:59 -0800

    Implement binary search to find target, mostly reusing search in rotated sorted code
    Signed-off-by: Arun <arun@arun.blog>

--- src/704-binary-search/main.go
+++ src/704-binary-search/main.go
@@ -1,5 +1,20 @@
 package main
 
 func search(nums []int, target int) int {
-	return 0
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if target >= nums[mid] {
+			if target == nums[mid] {
+				return mid
+			}
+			left = mid + 1
+		} else {
+			if target == nums[mid] {
+				return mid
+			}
+			right = mid - 1
+		}
+	}
+	return -1
 }
