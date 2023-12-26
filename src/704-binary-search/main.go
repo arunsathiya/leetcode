238c3ae965b6f740191ec27f45af85a39e8c35c7
Arun <arun@arun.blog>
2023-12-25 17:09:15 -0800

    Simplify target check to just once
    Signed-off-by: Arun <arun@arun.blog>

--- src/704-binary-search/main.go
+++ src/704-binary-search/main.go
@@ -4,15 +4,12 @@ func search(nums []int, target int) int {
 	left, right := 0, len(nums)-1
 	for left <= right {
 		mid := left + (right-left)/2
+		if target == nums[mid] {
+			return mid
+		}
 		if target >= nums[mid] {
-			if target == nums[mid] {
-				return mid
-			}
 			left = mid + 1
 		} else {
-			if target == nums[mid] {
-				return mid
-			}
 			right = mid - 1
 		}
 	}
