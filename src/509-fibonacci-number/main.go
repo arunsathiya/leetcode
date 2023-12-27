4dcc37dea9f939cb54ae9ebbb612e8b13f3cafcf
Arun <arun@arun.blog>
2023-12-26 18:18:02 -0800

    More improvements

--- src/509-fibonacci-number/main.go
+++ src/509-fibonacci-number/main.go
@@ -5,10 +5,9 @@ func fib(n int) int {
 		return n
 	}
 	sequence := make([]int, n)
-	sequence = append(sequence, 0)
-	sequence = append(sequence, 1)
+	sequence[0], sequence[1] = 0, 1
 	for i := 2; i < n; i++ {
-		sequence = append(sequence, sequence[i-1]+sequence[i-2])
+		sequence[i] = sequence[i-1] + sequence[i-2]
 	}
 	return sequence[n-1] + sequence[n-2]
 }
