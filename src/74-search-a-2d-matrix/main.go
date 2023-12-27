39b77d2eb6a4872e733aa7020b9cf1796c8ddbe3
Arun <arun@arun.blog>
2023-12-26 16:09:20 -0800

    Implement binary search but just for the first array of the matrix

--- src/74-search-a-2d-matrix/main.go
+++ src/74-search-a-2d-matrix/main.go
@@ -1,5 +1,18 @@
 package main
 
 func searchMatrix(matrix [][]int, target int) bool {
-	return true
+	firstmatrix := matrix[0]
+	left, right := 0, len(firstmatrix)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if target == firstmatrix[mid] {
+			return true
+		}
+		if firstmatrix[mid] >= firstmatrix[left] {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return false
 }
