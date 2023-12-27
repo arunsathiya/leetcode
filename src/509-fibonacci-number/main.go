aadf98eec50715dca5bc5d0ef5a400c175db8941
Arun <arun@arun.blog>
2023-12-26 18:16:27 -0800

    Back to original approach and simplify return for n<=1

--- src/509-fibonacci-number/main.go
+++ src/509-fibonacci-number/main.go
@@ -1,17 +1,14 @@
 package main
 
 func fib(n int) int {
-	if n == 0 {
-		return 0
+	if n <= 1 {
+		return n
 	}
-	if n == 1 {
-		return 1
+	sequence := make([]int, 0)
+	sequence = append(sequence, 0)
+	sequence = append(sequence, 1)
+	for i := 2; i < n; i++ {
+		sequence = append(sequence, sequence[i-1]+sequence[i-2])
 	}
-	a := 0
-	b := 1
-	for i := 1; i < n; i++ {
-		c := a + b
-		a, b = b, c
-	}
-	return b
+	return sequence[n-1] + sequence[n-2]
 }
