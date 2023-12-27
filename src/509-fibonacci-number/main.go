bf601ea4b68b97e299c49ccfef2ee238ef00021a
Arun <arun@arun.blog>
2023-12-26 18:21:15 -0800

    Revert back to a slice without a specified length and allow it to grow, it's the fastest solution

--- src/509-fibonacci-number/main.go
+++ src/509-fibonacci-number/main.go
@@ -4,10 +4,11 @@ func fib(n int) int {
 	if n <= 1 {
 		return n
 	}
-	sequence := make([]int, n)
-	sequence[0], sequence[1] = 0, 1
+	sequence := make([]int, 0)
+	sequence = append(sequence, 0)
+	sequence = append(sequence, 1)
 	for i := 2; i < n; i++ {
-		sequence[i] = sequence[i-1] + sequence[i-2]
+		sequence = append(sequence, sequence[i-1]+sequence[i-2])
 	}
 	return sequence[n-1] + sequence[n-2]
 }
