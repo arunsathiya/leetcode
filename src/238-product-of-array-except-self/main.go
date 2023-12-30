f7398521df340d0b811d9286c0ff67c54967a786
Arun <arun@arun.blog>
2023-12-29 17:19:28 -0800

    Some progress but realize it's clearly not an efficient approach and give up

--- src/238-product-of-array-except-self/main.go
+++ src/238-product-of-array-except-self/main.go
@@ -1,5 +1,24 @@
 package main
 
 func productExceptSelf(nums []int) []int {
-	return nil
+	if status, indices := containsZero(nums); status {
+		for index, num := range nums {
+			for zeroIndex, _ := range indices {
+				if index == zeroIndex {
+					nums[index] = 
+				}
+			}
+		}
+	}
+}
+
+func containsZero(nums []int) (bool, []int) {
+	indices := make([]int,0)
+	for i, num := range nums {
+		if num == 0 {
+			indices = append(indices, i)
+			return true, indices
+		}
+	}
+	return false, nil
 }
