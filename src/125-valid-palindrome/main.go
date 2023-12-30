bd3b258276093846741d0c4e15b1fac0f1aa0992
Arun <arun@arun.blog>
2023-12-29 19:06:22 -0800

    Use pointers to check palindrome status along with alphanumeric check

--- src/125-valid-palindrome/main.go
+++ src/125-valid-palindrome/main.go
@@ -1,5 +1,27 @@
 package main
 
+import "unicode"
+
 func isPalindrome(s string) bool {
+	s = alphanumeric(s)
+	left, right := 0, len(s)-1
+	for left < right {
+		if s[left] != s[right] {
+			return false
+		} else {
+			left++
+			right--
+		}
+	}
 	return true
+}
+
+func alphanumeric(s string) string {
+	var result []rune
+	for _, r := range s {
+		if ('A' <= r && r <= 'Z') || ('a' <= r && r <= 'z') || ('0' <= r && r <= '9') {
+			result = append(result, unicode.ToLower(r))
+		}
+	}
+	return string(result)
 }
