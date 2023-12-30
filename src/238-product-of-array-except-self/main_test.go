4eac591806f74ff0898bac9be10372669c222b3c
Arun <arun@arun.blog>
2023-12-29 17:06:49 -0800

    Setup test cases from leetcode

--- src/238-product-of-array-except-self/main_test.go
+++ src/238-product-of-array-except-self/main_test.go
@@ -1,6 +1,33 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestMain(t *testing.T) {}
-
+func TestMain(t *testing.T) {
+	tests := []struct {
+		name     string
+		nums     []int
+		expected []int
+	}{
+		{
+			name:     "first test case from leetcode",
+			nums:     []int{1, 2, 3, 4},
+			expected: []int{24, 12, 8, 6},
+		},
+		{
+			name:     "second test case from leetcode",
+			nums:     []int{-1, 1, 0, -3, 3},
+			expected: []int{0, 0, 9, 0, 0},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			output := productExceptSelf(test.nums)
+			if !reflect.DeepEqual(output, test.expected) {
+				t.Errorf("Expected %v, got %v", test.expected, output)
+			}
+		})
+	}
+}
