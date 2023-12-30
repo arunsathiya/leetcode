d0334f25f587d4d5f7e983702962ad93a7bff073
Arun <arun@arun.blog>
2023-12-29 17:49:44 -0800

    Calculate left product and right product for each element

--- src/238-product-of-array-except-self/main.go
+++ src/238-product-of-array-except-self/main.go
@@ -1,24 +1,27 @@
 package main
 
 func productExceptSelf(nums []int) []int {
-	if status, indices := containsZero(nums); status {
-		for index, num := range nums {
-			for zeroIndex, _ := range indices {
-				if index == zeroIndex {
-					nums[index] = 
-				}
-			}
-		}
+	if len(nums) == 0 {
+		return []int{}
 	}
-}
-
-func containsZero(nums []int) (bool, []int) {
-	indices := make([]int,0)
-	for i, num := range nums {
-		if num == 0 {
-			indices = append(indices, i)
-			return true, indices
-		}
+	output := make([]int, len(nums))
+	output[0] = 1
+	for i := 1; i < len(nums); i++ {
+		output[0] *= nums[i]
 	}
-	return false, nil
+	output[len(nums)-1] = 1
+	for i := 0; i < len(nums)-1; i++ {
+		output[len(nums)-1] *= nums[i]
+	}
+	for i := 1; i < len(nums)-1; i++ {
+		leftProduct, rightProduct := 1, 1
+		for j := 0; j < i; j++ {
+			leftProduct *= nums[j]
+		}
+		for j := i + 1; j < len(nums); j++ {
+			rightProduct *= nums[j]
+		}
+		output[i] = leftProduct * rightProduct
+	}
+	return output
 }
