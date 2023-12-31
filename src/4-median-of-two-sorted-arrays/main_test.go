9aa1c26d188c322c28203bfc081c2fd3016fd9bf
Arun <arun@arun.blog>
2023-12-31 14:28:49 -0800

    Setup test cases from leetcode

--- src/4-median-of-two-sorted-arrays/main_test.go
+++ src/4-median-of-two-sorted-arrays/main_test.go
@@ -2,5 +2,32 @@ package main
 
 import "testing"
 
-func TestMain(t *testing.T) {}
-
+func TestFindMedianSortedArrays(t *testing.T) {
+	tests := []struct {
+		name     string
+		nums1    []int
+		nums2    []int
+		expected float64
+	}{
+		{
+			name:     "first test case from leetcode",
+			nums1:    []int{1, 3},
+			nums2:    []int{2},
+			expected: 2.00000,
+		},
+		{
+			name:     "second test case from leetcode",
+			nums1:    []int{1, 2},
+			nums2:    []int{3, 4},
+			expected: 2.50000,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			output := findMedianSortedArrays(test.nums1, test.nums2)
+			if output != test.expected {
+				t.Errorf("Expected %v, got %v", test.expected, output)
+			}
+		})
+	}
+}
