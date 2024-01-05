ea08ec2ad218285edb055ce9e62235a2a9a6070b
Arun <arun@arun.blog>
2024-01-04 16:53:50 -0800

    Append to first slice, get sum and return median in float64 but this is incorrect in terms of time complexity

--- src/4-median-of-two-sorted-arrays/main.go
+++ src/4-median-of-two-sorted-arrays/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"sort"
+)
+
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	return 0
+	nums1 = append(nums1, nums2...)
+	sort.Ints(nums1)
+	sum := 0
+	for _, num := range nums1 {
+		sum += num
+	}
+	return float64(sum) / float64(len(nums1))
 }
