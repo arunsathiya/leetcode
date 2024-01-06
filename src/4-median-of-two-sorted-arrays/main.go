e17badc0c31c4f006d3135fdedf128733afc3d6b
Arun <arun@arun.blog>
2024-01-05 20:51:35 -0800

    Rework approach because we are talking about median, not mean. Add third test case from leetcode

--- src/4-median-of-two-sorted-arrays/main.go
+++ src/4-median-of-two-sorted-arrays/main.go
@@ -6,10 +6,8 @@ import (
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	nums1 = append(nums1, nums2...)
-	sort.Ints(nums1)
-	sum := 0
-	for _, num := range nums1 {
-		sum += num
-	}
-	return float64(sum) / float64(len(nums1))
+	sort.Slice(nums1, func(i, j int) bool {
+		return nums1[i] < nums1[j]
+	})
+	return 0
 }
