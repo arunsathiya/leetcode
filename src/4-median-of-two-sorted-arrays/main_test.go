86aa476a3e383ed2d45df8da6a595da831698238
Arun <arun@arun.blog>
2024-01-05 20:51:35 -0800

    Rework approach because we are talking about median, not mean. Add third test case from leetcode

--- src/4-median-of-two-sorted-arrays/main_test.go
+++ src/4-median-of-two-sorted-arrays/main_test.go
@@ -21,6 +21,12 @@ func TestFindMedianSortedArrays(t *testing.T) {
 			nums2:    []int{3, 4},
 			expected: 2.50000,
 		},
+		{
+			name:     "third test case from leetcode",
+			nums1:    []int{1, 3},
+			nums2:    []int{2, 7},
+			expected: 2.50000,
+		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
