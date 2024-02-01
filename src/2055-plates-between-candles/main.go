6abae9939ba12715adbaa8a32a6bebd2c4269334
Arun <arun@arun.blog>
2024-01-31 16:18:56 -0800

    Some progress but this is clearly wrong

--- src/2055-plates-between-candles/main.go
+++ src/2055-plates-between-candles/main.go
@@ -1,5 +1,21 @@
 package main
 
 func platesBetweenCandles(s string, queries [][]int) []int {
-	return nil
+	within := []int{}
+	for _, query := range queries {
+		ss := s[query[0]:query[1]]
+		count, inContainer := 0, false
+		for _, character := range ss {
+			switch character {
+			case '|':
+				inContainer = !inContainer
+			case '*':
+				if inContainer {
+					count++
+				}
+			}
+		}
+		within = append(within, count)
+	}
+	return within
 }
