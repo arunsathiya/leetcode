70d9673c060cbf59b3457b2fbb5dbbc2f6dbfd04
Arun <arun@arun.blog>
2024-02-13 11:59:21 -0800

    Loop through the nodes list until reaching the midpoint, and place the last values after the looped through values iteratively

--- src/143-reorder-list/main.go
+++ src/143-reorder-list/main.go
@@ -5,6 +5,24 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func ReorderList(head *ListNode) {
-
+func reorderList(head *ListNode) {
+	if head == nil || head.Next == nil {
+		return
+	}
+	var managed []*ListNode
+	for head != nil {
+		managed = append(managed, head)
+		head = head.Next
+	}
+	i, j := 0, len(managed)-1
+	for i < j {
+		managed[i].Next = managed[j]
+		i++
+		if i == j {
+			break
+		}
+		managed[j].Next = managed[i]
+		j--
+	}
+	managed[i].Next = nil
 }
