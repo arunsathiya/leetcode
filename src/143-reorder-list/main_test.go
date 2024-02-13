f201ed46ad416023a627c3093db3000f1de4a549
Arun <arun@arun.blog>
2024-02-12 23:42:05 -0800

    Setup test cases

--- src/143-reorder-list/main_test.go
+++ src/143-reorder-list/main_test.go
@@ -1,6 +1,43 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestMain(t *testing.T) {}
+func TestReverseList(t *testing.T) {
+	tests := []struct {
+		name  string
+		input *ListNode
+		want  []int
+	}{
+		{
+			name:  "5 elements",
+			input: &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}},
+			want:  []int{1, 5, 2, 4, 3},
+		},
+		{
+			name:  "4 elements",
+			input: &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, nil}}}},
+			want:  []int{1, 4, 2, 3},
+		},
+	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ReorderList(tt.input)
+			if !reflect.DeepEqual(ListNodeToSlice(got), tt.want) {
+				t.Errorf("Test %s failed: got %v, want %v", tt.name, ListNodeToSlice(got), tt.want)
+			}
+		})
+	}
+}
+
+func ListNodeToSlice(head *ListNode) []int {
+	var result []int
+	for head != nil {
+		result = append(result, head.Val)
+		head = head.Next
+	}
+	return result
+}
