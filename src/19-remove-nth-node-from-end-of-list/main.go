35fdbdb0a5bd6140dcd7a2be1ec3858765ec3581
Arun <arun@arun.blog>
2024-02-13 20:10:49 -0800

    Some progress but tests fail

--- src/19-remove-nth-node-from-end-of-list/main.go
+++ src/19-remove-nth-node-from-end-of-list/main.go
@@ -6,5 +6,19 @@ type ListNode struct {
 }
 
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-	return nil
+	var managed []*ListNode
+	for head != nil {
+		managed = append(managed, head)
+		head = head.Next
+	}
+	indexToRemove := len(managed) - n
+	if indexToRemove == 0 {
+		return head.Next
+	}
+	if indexToRemove > 0 && indexToRemove < len(managed) {
+		managed[indexToRemove-1].Next = managed[indexToRemove].Next
+	} else if indexToRemove > 0 { // If removing the last node, ensure the previous node points to nil
+		managed[indexToRemove-1].Next = nil
+	}
+	return head
 }
