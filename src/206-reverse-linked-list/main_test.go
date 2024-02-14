1c46a85dd87e292a4808d8e52d33c6a5582df6f2
Arun <arun@arun.blog>
2024-02-14 12:08:12 -0800

    Update tests to avoid converting to []int and natively use ListNode

--- src/206-reverse-linked-list/main_test.go
+++ src/206-reverse-linked-list/main_test.go
@@ -1,31 +1,35 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 )
+
+func isEqual(a, b *ListNode) bool {
+	for a != nil && b != nil {
+		if a.Val != b.Val {
+			return false
+		}
+		a = a.Next
+		b = b.Next
+	}
+	return a == nil && b == nil
+}
 
 func TestReverseList(t *testing.T) {
 	tests := map[string]struct {
 		input *ListNode
-		want  []int
+		want  *ListNode
 	}{
-		"reverse 5 elements": {&ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}}, []int{5, 4, 3, 2, 1}},
-		"reverse 2 elements": {&ListNode{1, &ListNode{2, nil}}, []int{2, 1}},
-		"reverse empty list": {nil, []int{}},
+		"reverse 5 elements": {&ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}}, &ListNode{5, &ListNode{4, &ListNode{3, &ListNode{2, &ListNode{1, nil}}}}}},
+		"reverse 2 elements": {&ListNode{1, &ListNode{2, nil}}, &ListNode{2, &ListNode{1, nil}}},
+		"reverse empty list": {nil, nil},
 	}
 
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
-			gotList := ReverseList(tt.input)
-			var got []int
-			for gotList != nil {
-				got = append(got, gotList.Val)
-				gotList = gotList.Next
-			}
-
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("%s: got %v, want %v", name, got, tt.want)
+			got := ReverseList(tt.input)
+			if !isEqual(got, tt.want) {
+				t.Errorf("%s: lists are not equal", name)
 			}
 		})
 	}
