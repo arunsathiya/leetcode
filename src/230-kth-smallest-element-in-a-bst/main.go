3e9638d8de1a99858769c48499d069560735e778
Arun <arun@arun.blog>
2024-02-16 12:55:49 -0800

    Recursive traversal from the left subtree to the right, to find the kth smallest

--- src/230-kth-smallest-element-in-a-bst/main.go
+++ src/230-kth-smallest-element-in-a-bst/main.go
@@ -7,5 +7,21 @@ type TreeNode struct {
 }
 
 func kthSmallest(root *TreeNode, k int) int {
-	return 0
+	count := 0
+	result := 0
+	var inOrder func(node *TreeNode)
+	inOrder = func(node *TreeNode) {
+		if node == nil || count >= k {
+			return
+		}
+		inOrder(node.Left)
+		count++
+		if count == k {
+			result = node.Val
+			return
+		}
+		inOrder(node.Right)
+	}
+	inOrder(root)
+	return result
 }
