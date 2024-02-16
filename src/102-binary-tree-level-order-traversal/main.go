3cfd3bdb6d21a6c17feb679c469fbcfe2efad594
Arun <arun@arun.blog>
2024-02-16 12:11:13 -0800

    Some progress

--- src/102-binary-tree-level-order-traversal/main.go
+++ src/102-binary-tree-level-order-traversal/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "container/list"
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -7,5 +9,29 @@ type TreeNode struct {
 }
 
 func levelOrder(root *TreeNode) [][]int {
-	return nil
+	if root == nil {
+		return [][]int{}
+	}
+	if root != nil && root.Left == nil && root.Right == nil {
+		return [][]int{{root.Val}}
+	}
+	deque := list.New()
+	deque.PushBack(root)
+	var outer [][]int
+	for deque.Len() > 0 {
+		queueLength := deque.Len()
+		var inner []int
+		for i := 0; i < queueLength; i++ {
+			node := deque.Front()
+			if node != nil {
+				inner = append(inner, int(node.Value))
+				deque.PushBack(node.Prev())
+				deque.PushBack(node.Next())
+			}
+		}
+		if len(inner) > 0 {
+			outer = append(outer, inner)
+		}
+	}
+	return outer
 }
