12c4e32389201f10e2426fe4518222795c446f51
Arun <arun@arun.blog>
2024-02-16 12:18:28 -0800

    Setup test cases

--- src/230-kth-smallest-element-in-a-bst/main_test.go
+++ src/230-kth-smallest-element-in-a-bst/main_test.go
@@ -1,6 +1,61 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestMain(t *testing.T) {}
+func TestKthSmallest(t *testing.T) {
+	tests := []struct {
+		root *TreeNode
+		k    int
+		want int
+	}{
+		{
+			root: &TreeNode{
+				Val: 3,
+				Left: &TreeNode{
+					Val: 1,
+					Right: &TreeNode{
+						Val: 2,
+					},
+				},
+				Right: &TreeNode{
+					Val: 4,
+				},
+			},
+			k:    1,
+			want: 1,
+		},
+		{
+			root: &TreeNode{
+				Val: 5,
+				Left: &TreeNode{
+					Val: 3,
+					Left: &TreeNode{
+						Val: 2,
+						Left: &TreeNode{
+							Val: 1,
+						},
+					},
+					Right: &TreeNode{
+						Val: 4,
+					},
+				},
+				Right: &TreeNode{
+					Val: 6,
+				},
+			},
+			k:    3,
+			want: 3,
+		},
+	}
 
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("Test with root %v and k %d", tt.root, tt.k), func(t *testing.T) {
+			if got := kthSmallest(tt.root, tt.k); got != tt.want {
+				t.Errorf("kthSmallest() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
