6312eaae8bf4a507f29a9761555e46b403dbab0e
Arun <arun@arun.blog>
2024-02-17 13:02:42 -0800

    Recursively validate if nodes are within upper and lower bounds and satisfy BST criteria

--- src/98-validate-binary-search-tree/main.go
+++ src/98-validate-binary-search-tree/main.go
@@ -1,11 +1,24 @@
 package main
 
+import "math"
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
 
+func isWithinRange(node *TreeNode, lower, upper int) bool {
+	if node == nil {
+		return true
+	}
+	if lower >= node.Val || upper <= node.Val {
+		return false
+	}
+	return isWithinRange(node.Left, lower, node.Val) && isWithinRange(node.Right, node.Val, upper)
+
+}
+
 func isValidBST(root *TreeNode) bool {
-	return false
+	return isWithinRange(root, math.MinInt, math.MaxInt)
 }
