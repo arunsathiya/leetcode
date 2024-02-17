dae282729a03c2b8e31967b15323e0038b5ae409
Arun <arun@arun.blog>
2024-02-17 12:39:23 -0800

    Setup test cases

--- src/98-validate-binary-search-tree/main_test.go
+++ src/98-validate-binary-search-tree/main_test.go
@@ -2,5 +2,47 @@ package main
 
 import "testing"
 
-func TestMain(t *testing.T) {}
+func TestIsValidBST(t *testing.T) {
+	tests := []struct {
+		root *TreeNode
+		want bool
+	}{
+		{
+			root: &TreeNode{
+				Val: 2,
+				Left: &TreeNode{
+					Val: 1,
+				},
+				Right: &TreeNode{
+					Val: 3,
+				},
+			},
+			want: true,
+		},
+		{
+			root: &TreeNode{
+				Val: 5,
+				Left: &TreeNode{
+					Val: 1,
+				},
+				Right: &TreeNode{
+					Val: 4,
+					Left: &TreeNode{
+						Val: 3,
+					},
+					Right: &TreeNode{
+						Val: 6,
+					},
+				},
+			},
+			want: false,
+		},
+	}
 
+	for _, tt := range tests {
+		got := isValidBST(tt.root)
+		if got != tt.want {
+			t.Errorf("isValidBST() = %v, want %v", got, tt.want)
+		}
+	}
+}
