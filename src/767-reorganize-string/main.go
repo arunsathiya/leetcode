bb22db662b73ca16de1dac22abe6213741f24be6
Arun <arun@arun.blog>
2024-02-17 22:32:36 -0800

    Return early if the character with the most occurances is more than half the string length (plus one for odd lengths)

--- src/767-reorganize-string/main.go
+++ src/767-reorganize-string/main.go
@@ -1,5 +1,18 @@
 package main
 
 func reorganizeString(s string) string {
+	hm := make(map[rune]int)
+	for _, char := range s {
+		hm[char]++
+	}
+	var maxCount int
+	for _, v := range hm {
+		if v > maxCount {
+			maxCount = v
+		}
+	}
+	if maxCount > (len(s)+1)/2 {
+		return ""
+	}
 	return ""
 }
