ce425bb36cd600cf03ae1ffefc18e97995bd6a22
Arun <arun@arun.blog>
2024-02-18 09:20:36 -0800

    Solve with hashmap and deterministic placement of frequent characters, without using heap or priority queue

--- src/767-reorganize-string/main.go
+++ src/767-reorganize-string/main.go
@@ -6,13 +6,35 @@ func reorganizeString(s string) string {
 		hm[char]++
 	}
 	var maxCount int
-	for _, v := range hm {
+	var maxChar rune
+	for k, v := range hm {
 		if v > maxCount {
 			maxCount = v
+			maxChar = k
 		}
 	}
+	// When no combination is possible
 	if maxCount > (len(s)+1)/2 {
 		return ""
 	}
-	return ""
+	// Fill even spaces
+	result := make([]rune, len(s))
+	index := 0
+	for hm[maxChar] > 0 {
+		result[index] = maxChar
+		index += 2
+		hm[maxChar]--
+	}
+	// Fill odd spaces
+	for k, v := range hm {
+		for v > 0 {
+			if index >= len(s) {
+				index = 1
+			}
+			result[index] = k
+			index += 2
+			v--
+		}
+	}
+	return string(result)
 }
