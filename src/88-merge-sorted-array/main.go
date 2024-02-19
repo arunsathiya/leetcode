900e5d213ccaf25cc86f384734d35d34f8d6809f
Arun <arun@arun.blog>
2024-02-19 14:17:39 -0800

    Place nums2 values into the last indices marked as 0 on nums1, and then sort

--- src/88-merge-sorted-array/main.go
+++ src/88-merge-sorted-array/main.go
@@ -1,5 +1,12 @@
 package main
 
-func merge(nums1 []int, m int, nums2 []int, n int) {
+import "sort"
 
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	if n > 0 {
+		for i := 0; i < len(nums2); i++ {
+			nums1[m+i] = nums2[i]
+		}
+	}
+	sort.Ints(nums1)
 }
