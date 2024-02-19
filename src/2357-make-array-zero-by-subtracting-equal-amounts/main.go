7d626b3d67a9b464cad171a6392567a61e53a0e6
Arun <arun@arun.blog>
2024-02-19 13:23:27 -0800

    Clever solution from Leetcode, identify count of non-zero unique elements in the array and return as the minimum number of operations

--- src/2357-make-array-zero-by-subtracting-equal-amounts/main.go
+++ src/2357-make-array-zero-by-subtracting-equal-amounts/main.go
@@ -1,38 +1,11 @@
 package main
 
-import "sort"
-
-func allZeroes(slice []int) bool {
-	for _, v := range slice {
-		if v != 0 {
-			return false
-		}
-	}
-	return true
-}
-
-func subtractByValue(slice []int, value int) []int {
-	for i := range slice {
-		if slice[i] > 0 {
-			slice[i] -= value
-		}
-	}
-	return slice
-}
-
 func minimumOperations(nums []int) int {
-	count := 0
-	for !allZeroes(nums) {
-		sort.Ints(nums)
-		var smallest int
-		for _, v := range nums {
-			if v > 0 {
-				smallest = v
-				break
-			}
+	tmp := make(map[int]int)
+	for _, num := range nums {
+		if num != 0 {
+			tmp[num]++
 		}
-		nums = subtractByValue(nums, smallest)
-		count++
 	}
-	return count
+	return len(tmp)
 }
