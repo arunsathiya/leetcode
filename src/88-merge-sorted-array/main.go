b3087319515e88aec3be437e073f54c4bd003bba
Arun <arun@arun.blog>
2024-02-19 14:49:13 -0800

    Improve merge efficiency in linear time without requiring a full sort after the merge as in previous approach

--- src/88-merge-sorted-array/main.go
+++ src/88-merge-sorted-array/main.go
@@ -1,12 +1,15 @@
 package main
 
-import "sort"
-
 func merge(nums1 []int, m int, nums2 []int, n int) {
-	if n > 0 {
-		for i := 0; i < len(nums2); i++ {
-			nums1[m+i] = nums2[i]
+	i1, i2, mi := m-1, n-1, m+n-1
+	for i2 >= 0 {
+		if i1 >= 0 && nums1[i1] > nums2[i2] {
+			nums1[mi] = nums1[i1]
+			i1--
+		} else {
+			nums1[mi] = nums2[i2]
+			i2--
 		}
+		mi--
 	}
-	sort.Ints(nums1)
 }
