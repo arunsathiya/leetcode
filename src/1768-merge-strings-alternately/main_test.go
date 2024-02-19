4381c14c32fe398402e08c4916f64ccb8bb28319
Arun <arun@arun.blog>
2024-02-19 14:53:15 -0800

    Setup test cases

--- src/1768-merge-strings-alternately/main_test.go
+++ src/1768-merge-strings-alternately/main_test.go
@@ -2,5 +2,21 @@ package main
 
 import "testing"
 
-func TestMain(t *testing.T) {}
+func TestMergeAlternately(t *testing.T) {
+	tests := []struct {
+		word1 string
+		word2 string
+		want  string
+	}{
+		{"abc", "pqr", "apbqcr"},
+		{"ab", "pqrs", "apbqrs"},
+		{"abcd", "pq", "apbqcd"},
+	}
 
+	for _, tt := range tests {
+		got := mergeAlternately(tt.word1, tt.word2)
+		if got != tt.want {
+			t.Errorf("mergeAlternately(%q, %q) = %q, want %q", tt.word1, tt.word2, got, tt.want)
+		}
+	}
+}
