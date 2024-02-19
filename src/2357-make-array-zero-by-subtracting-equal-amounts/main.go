7d1c18d5d8dfa4405440b14623cb13eb91ad3341
Arun <arun@arun.blog>
2024-02-19 13:17:49 -0800

    Solve problem

--- src/2357-make-array-zero-by-subtracting-equal-amounts/main.go
+++ src/2357-make-array-zero-by-subtracting-equal-amounts/main.go
@@ -1,5 +1,38 @@
 package main
 
+import "sort"
+
+func allZeroes(slice []int) bool {
+	for _, v := range slice {
+		if v != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func subtractByValue(slice []int, value int) []int {
+	for i := range slice {
+		if slice[i] > 0 {
+			slice[i] -= value
+		}
+	}
+	return slice
+}
+
 func minimumOperations(nums []int) int {
-	return 0
+	count := 0
+	for !allZeroes(nums) {
+		sort.Ints(nums)
+		var smallest int
+		for _, v := range nums {
+			if v > 0 {
+				smallest = v
+				break
+			}
+		}
+		nums = subtractByValue(nums, smallest)
+		count++
+	}
+	return count
 }
