9957279a70ca38e09a676a3172354a2f8b91dcd1
Arun <arun@arun.blog>
2024-02-19 15:01:58 -0800

    Solve problem but isn't efficient

--- src/1768-merge-strings-alternately/main.go
+++ src/1768-merge-strings-alternately/main.go
@@ -1,5 +1,19 @@
 package main
 
 func mergeAlternately(word1 string, word2 string) string {
-	return ""
+	var result string
+	if len(word1) <= len(word2) {
+		for i := 0; i < len(word1); i++ {
+			result += string(word1[i])
+			result += string(word2[i])
+		}
+		result += string(word2[len(word1):])
+	} else {
+		for i := 0; i < len(word2); i++ {
+			result += string(word1[i])
+			result += string(word2[i])
+		}
+		result += string(word1[len(word2):])
+	}
+	return result
 }
