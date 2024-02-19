dc63d2f9fefc6b395cf89b2cafc38430a54c1f54
Arun <arun@arun.blog>
2024-02-19 15:15:14 -0800

    Setup test cases

--- src/1492-the-kth-factor-of-n/main_test.go
+++ src/1492-the-kth-factor-of-n/main_test.go
@@ -2,5 +2,21 @@ package main
 
 import "testing"
 
-func TestMain(t *testing.T) {}
+func TestKthFactor(t *testing.T) {
+	testCases := []struct {
+		n        int
+		k        int
+		expected int
+	}{
+		{12, 3, 3},
+		{7, 2, 7},
+		{4, 4, -1},
+	}
 
+	for _, tc := range testCases {
+		result := kthFactor(tc.n, tc.k)
+		if result != tc.expected {
+			t.Errorf("For n = %d, k = %d, expected %d but got %d", tc.n, tc.k, tc.expected, result)
+		}
+	}
+}
