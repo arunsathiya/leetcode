ebb9865b0bc5cdffb72ead3eb5348bf574abdd7f
Arun <arun@arun.blog>
2024-02-20 11:39:22 -0800

    Compare if all characters are of the same count, and if the total number of characters neatly divides by the length of words slice

--- src/1897-redistribute-characters-to-make-all-strings-equal/main.go
+++ src/1897-redistribute-characters-to-make-all-strings-equal/main.go
@@ -1,5 +1,31 @@
 package main
 
+import "strings"
+
 func makeEqual(words []string) bool {
+	hm := make(map[string]int)
+	count := 0
+	for _, word := range words {
+		characters := strings.Split(word, "")
+		count += len(characters)
+		for _, char := range characters {
+			hm[char]++
+		}
+	}
+	if count%len(words) == 0 && areCharCountsEqual(hm) {
+		return true
+	}
+	return false
+}
+
+func areCharCountsEqual(hm map[string]int) bool {
+	var prevCount int
+	for _, count := range hm {
+		if prevCount == 0 {
+			prevCount = count
+		} else if prevCount != count {
+			return false
+		}
+	}
 	return true
 }
