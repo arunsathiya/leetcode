1d7a909208f88bdd5cd0af2feef1c3cca2e0a0be
Arun <arun@arun.blog>
2024-02-20 12:12:20 -0800

    Solve by ensuring that each character's total count is divisible by the number of words, so that those characters can be equally placed in each word

--- src/1897-redistribute-characters-to-make-all-strings-equal/main.go
+++ src/1897-redistribute-characters-to-make-all-strings-equal/main.go
@@ -1,29 +1,14 @@
 package main
 
-import "strings"
-
 func makeEqual(words []string) bool {
-	hm := make(map[string]int)
-	count := 0
+	hm := make(map[rune]int)
 	for _, word := range words {
-		characters := strings.Split(word, "")
-		count += len(characters)
-		for _, char := range characters {
+		for _, char := range word {
 			hm[char]++
 		}
 	}
-	if count%len(words) == 0 && areCharCountsEqual(hm) {
-		return true
-	}
-	return false
-}
-
-func areCharCountsEqual(hm map[string]int) bool {
-	var prevCount int
 	for _, count := range hm {
-		if prevCount == 0 {
-			prevCount = count
-		} else if prevCount != count {
+		if count%len(words) != 0 {
 			return false
 		}
 	}
