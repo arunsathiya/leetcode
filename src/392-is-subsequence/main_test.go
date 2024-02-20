86b657a6f510f23f39cc933f89b3ac300620697e
Arun <arun@arun.blog>
2024-02-20 15:53:29 -0800

    Setup test cases

--- src/392-is-subsequence/main_test.go
+++ src/392-is-subsequence/main_test.go
@@ -2,5 +2,19 @@ package main
 
 import "testing"
 
-func TestMain(t *testing.T) {}
+func TestIsSubsequence(t *testing.T) {
+	tests := []struct {
+		s        string
+		t        string
+		expected bool
+	}{
+		{"abc", "ahbgdc", true},
+		{"axc", "ahbgdc", false},
+	}
 
+	for _, test := range tests {
+		if result := isSubsequence(test.s, test.t); result != test.expected {
+			t.Errorf("isSubsequence(%q, %q) = %v; want %v", test.s, test.t, result, test.expected)
+		}
+	}
+}
