e993661e10e0ad569620311fc926490d2532b2ca
Arun <arun@arun.blog>
2024-02-19 17:26:17 -0800

    Solve problem after sorting slice

--- src/1492-the-kth-factor-of-n/main.go
+++ src/1492-the-kth-factor-of-n/main.go
@@ -1,5 +1,27 @@
 package main
 
+import "sort"
+
 func kthFactor(n int, k int) int {
-	return 0
+	factors := make(map[int]bool)
+	for i := 1; i <= n; i++ {
+		if n%i == 0 {
+			if _, exists := factors[i]; !exists {
+				factors[i] = true
+				factors[n/i] = true
+			} else {
+				break
+			}
+		}
+	}
+	var s []int
+	for k := range factors {
+		s = append(s, k)
+	}
+	sort.Ints(s)
+	if k <= len(s) {
+		return s[k-1]
+	} else {
+		return -1
+	}
 }
