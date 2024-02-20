fc0b6d9c66bed5a4a240f587c0495517bfe0d45c
Arun <arun@arun.blog>
2024-02-20 13:05:56 -0800

    Setup test cases

--- src/13-roman-to-integer/main_test.go
+++ src/13-roman-to-integer/main_test.go
@@ -2,5 +2,20 @@ package main
 
 import "testing"
 
-func TestMain(t *testing.T) {}
+func TestRomanToInt(t *testing.T) {
+	cases := []struct {
+		s        string
+		expected int
+	}{
+		{"III", 3},
+		{"LVIII", 58},
+		{"MCMXCIV", 1994},
+	}
 
+	for _, c := range cases {
+		got := romanToInt(c.s)
+		if got != c.expected {
+			t.Errorf("romanToInt(%q) == %d, want %d", c.s, got, c.expected)
+		}
+	}
+}
