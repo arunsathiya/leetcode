fe743dd57b12f2daf033618b9e33444f18e6ce91
Arun <arun@arun.blog>
2024-02-20 22:58:09 -0800

    Some progress

--- src/2405-optimal-partition-of-string/main.go
+++ src/2405-optimal-partition-of-string/main.go
@@ -1,5 +1,14 @@
 package main
 
 func partitionString(s string) int {
-	return 0
+	optimal := make([]string, 0)
+	for len(s) > 0 {
+		instance := ""
+		for s[0] != instance[len(instance)-1] {
+			instance += string(s[0])
+			s = s[1:]
+		}
+		optimal = append(optimal, instance)
+	}
+	return len(optimal)
 }
