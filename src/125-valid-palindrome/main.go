c6df0b2177f9a85dfaf25a1785f3cd4703ecfee8
Arun <arun@arun.blog>
2024-02-21 10:58:31 -0800

    Solve by comparing if the first and last characters are the same, stepping to the next character iteratively

--- src/125-valid-palindrome/main.go
+++ src/125-valid-palindrome/main.go
@@ -1,5 +1,22 @@
 package main
 
 func isPalindrome(s string) bool {
-	return false
+	managed := ""
+	for _, char := range s {
+		if (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9') {
+			if char >= 'A' && char <= 'Z' {
+				char += 32
+			}
+			managed += string(char)
+		}
+	}
+	left, right := 0, len(managed)-1
+	for left < right {
+		if managed[left] != managed[right] {
+			return false
+		}
+		left++
+		right--
+	}
+	return true
 }
