4d566e625eb5f4b8bf11b12342b2302f38c239b2
Arun <arun@arun.blog>
2024-02-20 23:41:44 -0800

    Setup problem folder

--- src/1-two-sum/main.go
+++ src/1-two-sum/main.go
@@ -1,12 +1,4 @@
 package main
 
-func twoSum(nums []int, target int) []int {
-	hashmap := make(map[int]int)
-	for i, num := range nums {
-		if j, okay := hashmap[target-num]; okay {
-			return []int{j, i}
-		}
-		hashmap[num] = i
-	}
-	return nil
-}
+func main() {}
+
--- src/1-two-sum/main_test.go
+++ src/1-two-sum/main_test.go
@@ -1,36 +1,6 @@
 package main
 
-import (
-	"reflect"
-	"testing"
-)
+import "testing"
 
-func TestMain(t *testing.T) {
-	tests := []struct {
-		name     string
-		input    []int
-		target   int
-		expected []int
-	}{
-		{
-			name:     "test case from leetcode",
-			input:    []int{2, 7, 11, 15},
-			target:   9,
-			expected: []int{0, 1},
-		},
-		{
-			name:     "test case from leetcode",
-			input:    []int{3, 2, 4},
-			target:   6,
-			expected: []int{1, 2},
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			output := twoSum(test.input, test.target)
-			if !reflect.DeepEqual(test.expected, output) {
-				t.Errorf("Expected %v, got %v", test.expected, output)
-			}
-		})
-	}
-}
+func TestMain(t *testing.T) {}
+
