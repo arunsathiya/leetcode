bf5b8955bc0ed4f91123f81cda1fb92bcd51d259
Arun <arun@arun.blog>
2024-02-20 16:34:50 -0800

    Setup test cases

--- src/268-missing-number/main.go
+++ src/268-missing-number/main.go
@@ -1,4 +1,5 @@
 package main
 
-func main() {}
-
+func missingNumber(nums []int) int {
+	return 0
+}
--- src/268-missing-number/main_test.go
+++ src/268-missing-number/main_test.go
@@ -2,5 +2,20 @@ package main
 
 import "testing"
 
-func TestMain(t *testing.T) {}
+func TestMissingNumber(t *testing.T) {
+	testCases := []struct {
+		nums []int
+		want int
+	}{
+		{[]int{3, 0, 1}, 2},
+		{[]int{0, 1}, 2},
+		{[]int{9, 6, 4, 2, 3, 5, 7, 0, 1}, 8},
+	}
 
+	for _, tc := range testCases {
+		got := missingNumber(tc.nums)
+		if got != tc.want {
+			t.Errorf("For input %v; expected %d but got %d", tc.nums, tc.want, got)
+		}
+	}
+}
