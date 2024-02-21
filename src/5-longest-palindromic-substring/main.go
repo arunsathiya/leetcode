6fc85594c2805cdada60e600f19d146a327f06f7
Arun <arun@arun.blog>
2024-02-21 10:38:28 -0800

    Solve with palindromic substrings approach, but capture longest palindrome by comparing if the indices are greater than the previous set of indices

--- src/5-longest-palindromic-substring/main.go
+++ src/5-longest-palindromic-substring/main.go
@@ -1,5 +1,23 @@
 package main
 
 func longestPalindrome(s string) string {
-	return ""
+	if s == "" {
+		return ""
+	}
+	start, end := 0, 0
+	var expandAroundCenter = func(left int, right int) {
+		for left >= 0 && right < len(s) && s[left] == s[right] {
+			if (right - left) > (end - start) {
+				start = left
+				end = right
+			}
+			left--
+			right++
+		}
+	}
+	for i := 0; i < len(s); i++ {
+		expandAroundCenter(i, i)
+		expandAroundCenter(i, i+1)
+	}
+	return s[start : end+1]
 }
