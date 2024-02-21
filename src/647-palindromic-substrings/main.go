c43a47258987f744815a472070c15ed1e9e1893b
Arun <arun@arun.blog>
2024-02-21 09:57:54 -0800

    Validate upto two character palindromes

--- src/647-palindromic-substrings/main.go
+++ src/647-palindromic-substrings/main.go
@@ -1,5 +1,17 @@
 package main
 
 func countSubstrings(s string) int {
-	return 0
+	if s == "" {
+		return 0
+	}
+	palindromicSubstrings := make([]string, 0)
+	for _, char := range s {
+		palindromicSubstrings = append(palindromicSubstrings, string(char))
+	}
+	for i := 0; i < len(s)-1; i++ {
+		if s[i] == s[i+1] {
+			palindromicSubstrings = append(palindromicSubstrings, s[i:i+2])
+		}
+	}
+	return len(palindromicSubstrings)
 }
