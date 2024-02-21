7d6bcd619c88100c16da41d2bf99fcbedfd8e6c6
Arun <arun@arun.blog>
2024-02-20 23:42:20 -0800

    Setup test cases

--- src/1-two-sum/main_test.go
+++ src/1-two-sum/main_test.go
@@ -1,6 +1,42 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestMain(t *testing.T) {}
+func TestTwoSum(t *testing.T) {
+	tests := []struct {
+		name   string
+		nums   []int
+		target int
+		want   []int
+	}{
+		{
+			name:   "Example 1",
+			nums:   []int{2, 7, 11, 15},
+			target: 9,
+			want:   []int{0, 1},
+		},
+		{
+			name:   "Example 2",
+			nums:   []int{3, 2, 4},
+			target: 6,
+			want:   []int{1, 2},
+		},
+		{
+			name:   "Example 3",
+			nums:   []int{3, 3},
+			target: 6,
+			want:   []int{0, 1},
+		},
+	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoSum(tt.nums, tt.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
