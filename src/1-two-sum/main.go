9c630e6122bb2b404a339af9e516f88fde6c3962
Arun <arun@arun.blog>
2024-02-21 09:35:13 -0800

    Use hashmap to look up presence of difference of target and num, and return indices

--- src/1-two-sum/main.go
+++ src/1-two-sum/main.go
@@ -1,12 +1,12 @@
 package main
 
 func twoSum(nums []int, target int) []int {
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				return []int{i, j}
-			}
+	hm := make(map[int]int)
+	for i, num := range nums {
+		if j, okay := hm[target-num]; okay {
+			return []int{j, i}
 		}
+		hm[num] = i
 	}
 	return nil
 }
