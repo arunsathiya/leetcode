80936c820ceb7275f94c04fe55a3e7552abc919c
Arun <arun@arun.blog>
2024-02-20 16:52:50 -0800

    Calculate sum of first n numbers, and subtract each number in the slice to find the missing number

--- src/268-missing-number/main.go
+++ src/268-missing-number/main.go
@@ -1,20 +1,10 @@
 package main
 
-import "sort"
-
 func missingNumber(nums []int) int {
 	n := len(nums)
-	sort.Ints(nums)
-	j := n - 1
-	for i := 0; i < n && j < n; i++ {
-		if nums[i]+nums[j] != n {
-			if nums[i] != nums[i-1]+1 {
-				return nums[i-1] + 1
-			}
-			if nums[j] != nums[j+1]-1 {
-				return nums[j+1] - 1
-			}
-		}
+	sum := n * ((n + 1) / 2)
+	for _, num := range nums {
+		sum -= num
 	}
-	return 0
+	return sum
 }
