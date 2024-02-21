0a4ed95d167a1375b422a42b1c793ee906eecf23
Arun <arun@arun.blog>
2024-02-21 10:47:53 -0800

    Setup test cases

--- src/125-valid-palindrome/main.go
+++ src/125-valid-palindrome/main.go
@@ -1,25 +1,5 @@
 package main
 
-import (
-	"strings"
-	"unicode"
-)
-
 func isPalindrome(s string) bool {
-	f := func(r rune) rune {
-		if !unicode.IsLetter(r) && !unicode.IsNumber(r) {
-			return -1
-		}
-		return unicode.ToLower(r)
-	}
-	s = strings.Map(f, s)
-	left, right := 0, len(s)-1
-	for left < right {
-		if s[left] != s[right] {
-			return false
-		}
-		left++
-		right--
-	}
-	return true
+	return false
 }
