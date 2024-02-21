dc54c542d22dde8efa2f8810a063ddf71d3d40b5
Arun <arun@arun.blog>
2024-02-21 09:32:15 -0800

    Brute-force solution

--- src/1-two-sum/main.go
+++ src/1-two-sum/main.go
@@ -1,5 +1,12 @@
 package main
 
 func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
 	return nil
 }
