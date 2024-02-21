eddfee7f5417257af44b441f544c3d39f0967649
Arun <arun@arun.blog>
2024-02-20 16:51:03 -0800

    Juggling with two pointers and obviously broken, out-of-bounds solution. Need to rework a different approach

--- src/268-missing-number/main.go
+++ src/268-missing-number/main.go
@@ -1,5 +1,20 @@
 package main
 
+import "sort"
+
 func missingNumber(nums []int) int {
+	n := len(nums)
+	sort.Ints(nums)
+	j := n - 1
+	for i := 0; i < n && j < n; i++ {
+		if nums[i]+nums[j] != n {
+			if nums[i] != nums[i-1]+1 {
+				return nums[i-1] + 1
+			}
+			if nums[j] != nums[j+1]-1 {
+				return nums[j+1] - 1
+			}
+		}
+	}
 	return 0
 }
