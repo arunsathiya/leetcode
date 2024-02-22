adece23428925801f9c1034ecc2e544ff0b1cc19
Arun <arun@arun.blog>
2024-02-21 20:31:01 -0800

    Calculate valid 101 and 010 combos

--- src/2222-number-of-ways-to-select-buildings/main.go
+++ src/2222-number-of-ways-to-select-buildings/main.go
@@ -1,5 +1,27 @@
 package main
 
 func numberOfWays(s string) int64 {
-	return 0
+	totalZeroes := 0
+	totalOnes := 0
+	countZeroesSoFar := 0
+	countOnesSoFar := 0
+	valid101Combos := 0
+	valid010Combos := 0
+	for _, char := range s {
+		if char == '0' {
+			totalZeroes++
+		} else {
+			totalOnes++
+		}
+	}
+	for _, char := range s {
+		if char == '0' {
+			countZeroesSoFar++
+			valid101Combos += countOnesSoFar * (totalOnes - countOnesSoFar)
+		} else {
+			countOnesSoFar++
+			valid010Combos += countZeroesSoFar * (totalZeroes - countZeroesSoFar)
+		}
+	}
+	return int64(valid010Combos + valid101Combos)
 }
