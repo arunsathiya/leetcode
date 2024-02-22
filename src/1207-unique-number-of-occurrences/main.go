751d4af9c4afecc89020133ce2415a8af419c295
Arun <arun@arun.blog>
2024-02-22 09:26:50 -0800

    Solve with hashmap

--- src/1207-unique-number-of-occurrences/main.go
+++ src/1207-unique-number-of-occurrences/main.go
@@ -1,5 +1,16 @@
 package main
 
 func uniqueOccurrences(arr []int) bool {
-	return false
+	hm := make(map[int]int)
+	for _, num := range arr {
+		hm[num]++
+	}
+	values := make(map[int]int)
+	for _, v := range hm {
+		values[v]++
+		if values[v] > 1 {
+			return false
+		}
+	}
+	return true
 }
