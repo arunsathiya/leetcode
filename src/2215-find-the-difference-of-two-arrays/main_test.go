d3df827e415a038551c581d8de2802f8616149b4
Arun <arun@arun.blog>
2024-02-22 08:14:36 -0800

    Setup test cases

--- src/2215-find-the-difference-of-two-arrays/main_test.go
+++ src/2215-find-the-difference-of-two-arrays/main_test.go
@@ -1,6 +1,36 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestMain(t *testing.T) {}
+func TestFindDifference(t *testing.T) {
+	tests := []struct {
+		name  string
+		nums1 []int
+		nums2 []int
+		want  [][]int
+	}{
+		{
+			name:  "Example 1",
+			nums1: []int{1, 2, 3},
+			nums2: []int{2, 4, 6},
+			want:  [][]int{{1, 3}, {4, 6}},
+		},
+		{
+			name:  "Example 2",
+			nums1: []int{1, 2, 3, 3},
+			nums2: []int{1, 1, 2, 2},
+			want:  [][]int{{3}, {}},
+		},
+	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findDifference(tt.nums1, tt.nums2); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findDifference() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
