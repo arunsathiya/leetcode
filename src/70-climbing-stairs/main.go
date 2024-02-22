e208afd8bc33c21fa56d2c51b374e7d1f644a943
Arun <arun@arun.blog>
2024-02-21 18:56:45 -0800

    Setup test cases

--- src/70-climbing-stairs/main.go
+++ src/70-climbing-stairs/main.go
@@ -1,4 +1,5 @@
 package main
 
-func main() {}
-
+func climbStairs(n int) int {
+	return 0
+}
--- src/70-climbing-stairs/main_test.go
+++ src/70-climbing-stairs/main_test.go
@@ -2,5 +2,22 @@ package main
 
 import "testing"
 
-func TestMain(t *testing.T) {}
+func TestClimbStairs(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    int
+		expected int
+	}{
+		{"2 steps", 2, 2},
+		{"3 steps", 3, 3},
+	}
 
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := climbStairs(tc.input)
+			if result != tc.expected {
+				t.Errorf("For input '%d', expected '%d' but got '%d'", tc.input, tc.expected, result)
+			}
+		})
+	}
+}
