2d69da6e472803361c2612dd909a44fd363fa558
Arun <arun@arun.blog>
2024-02-21 19:26:01 -0800

    Some progress but this is clearly wrong

--- src/70-climbing-stairs/main.go
+++ src/70-climbing-stairs/main.go
@@ -1,5 +1,15 @@
 package main
 
 func climbStairs(n int) int {
-	return 0
+	if n == 1 {
+		return 1
+	}
+	if n == 2 {
+		return 2
+	}
+	ways := make([]int, n+1)
+	for i := 3; i <= n; i++ {
+		ways[i] = ways[i-1] + ways[i-2]
+	}
+	return ways[n]
 }
