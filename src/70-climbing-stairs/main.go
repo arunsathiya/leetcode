ee5cf84abc5f46e73ac8f2cba9a5956da051a1b1
Arun <arun@arun.blog>
2024-02-21 19:34:31 -0800

    Use a slice instead of a map with a specified storage capacity because the problem statement defines the maximum to be 45 stairs

--- src/70-climbing-stairs/main.go
+++ src/70-climbing-stairs/main.go
@@ -1,15 +1,15 @@
 package main
 
 func climbStairs(n int) int {
-	memo := make(map[int]int, 0)
+	memo := make([]int, 46)
 	return climbStairsMemo(n, memo)
 }
 
-func climbStairsMemo(n int, memo map[int]int) int {
+func climbStairsMemo(n int, memo []int) int {
 	if n <= 2 {
 		return n
 	}
-	if _, exists := memo[n]; exists {
+	if memo[n] != 0 {
 		return memo[n]
 	}
 	memo[n] = climbStairsMemo(n-1, memo) + climbStairsMemo(n-2, memo)
