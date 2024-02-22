9c828094700e554c87fe51827b7eb3f8c2cb7f1e
Arun <arun@arun.blog>
2024-02-21 20:39:35 -0800

    Another approach of tracking 01s and 10s to count the total way, h/t Leetcode discussion

--- src/2222-number-of-ways-to-select-buildings/main.go
+++ src/2222-number-of-ways-to-select-buildings/main.go
@@ -1,27 +1,19 @@
 package main
 
 func numberOfWays(s string) int64 {
-	totalZeroes := 0
-	totalOnes := 0
-	countZeroesSoFar := 0
-	countOnesSoFar := 0
-	valid101Combos := 0
-	valid010Combos := 0
+	count0, count1 := 0, 0
+	count01, count10 := 0, 0
+	ways := 0
 	for _, char := range s {
 		if char == '0' {
-			totalZeroes++
+			ways += count01
+			count10 += count1
+			count0++
 		} else {
-			totalOnes++
+			ways += count10
+			count01 += count0
+			count1++
 		}
 	}
-	for _, char := range s {
-		if char == '0' {
-			countZeroesSoFar++
-			valid101Combos += countOnesSoFar * (totalOnes - countOnesSoFar)
-		} else {
-			countOnesSoFar++
-			valid010Combos += countZeroesSoFar * (totalZeroes - countZeroesSoFar)
-		}
-	}
-	return int64(valid010Combos + valid101Combos)
+	return int64(ways)
 }
