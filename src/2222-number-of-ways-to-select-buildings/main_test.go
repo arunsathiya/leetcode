86ad6e113e1f533e6652b78e61f3b1f59a018900
Arun <arun@arun.blog>
2024-02-21 19:47:46 -0800

    Setup test cases

--- src/2222-number-of-ways-to-select-buildings/main_test.go
+++ src/2222-number-of-ways-to-select-buildings/main_test.go
@@ -2,5 +2,18 @@ package main
 
 import "testing"
 
-func TestMain(t *testing.T) {}
+func TestNumberOfWays(t *testing.T) {
+	tests := []struct {
+		s        string
+		expected int64
+	}{
+		{"001101", 6},
+		{"11100", 0},
+	}
 
+	for _, test := range tests {
+		if got := numberOfWays(test.s); got != test.expected {
+			t.Errorf("numberOfWays(%q) = %d; want %d", test.s, got, test.expected)
+		}
+	}
+}
