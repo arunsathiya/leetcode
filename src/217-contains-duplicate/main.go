5ffcd749ac0d1a77f0c26330d6b7d0d6319f11cf
Arun <arun@arun.blog>
2024-02-23 11:04:58 -0800

    Solve with hashmap

--- src/217-contains-duplicate/main.go
+++ src/217-contains-duplicate/main.go
@@ -1,5 +1,12 @@
 package main
 
 func containsDuplicate(nums []int) bool {
+	hm := make(map[int]bool)
+	for _, num := range nums {
+		if hm[num] {
+			return true
+		}
+		hm[num] = true
+	}
 	return false
 }
