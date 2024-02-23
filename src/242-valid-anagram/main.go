2007fb64fac2905fb7cafdf79f040d2d1ce710cb
Arun <arun@arun.blog>
2024-02-23 11:12:44 -0800

    Setup test cases

--- src/242-valid-anagram/main.go
+++ src/242-valid-anagram/main.go
@@ -1,4 +1,5 @@
 package main
 
-func main() {}
-
+func isAnagram(s string, t string) bool {
+	return false
+}
--- src/242-valid-anagram/main_test.go
+++ src/242-valid-anagram/main_test.go
@@ -2,5 +2,20 @@ package main
 
 import "testing"
 
-func TestMain(t *testing.T) {}
+func TestIsAnagram(t *testing.T) {
+	testCases := []struct {
+		s        string
+		t        string
+		expected bool
+	}{
+		{"anagram", "nagaram", true},
+		{"rat", "car", false},
+	}
 
+	for _, tc := range testCases {
+		actual := isAnagram(tc.s, tc.t)
+		if actual != tc.expected {
+			t.Errorf("isAnagram(%v, %v) = %v, expected %v", tc.s, tc.t, actual, tc.expected)
+		}
+	}
+}
