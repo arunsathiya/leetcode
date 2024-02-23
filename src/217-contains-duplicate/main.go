98955e22b04250f87aa30d902d2fdafce66cf45b
Arun <arun@arun.blog>
2024-02-23 11:10:26 -0800

    Intentional typo to figure out why local and remote branches are out of date, yet git push shows up to date

--- src/217-contains-duplicate/main.go
+++ src/217-contains-duplicate/main.go
@@ -3,7 +3,7 @@ package main
 func containsDuplicate(nums []int) bool {
 	hm := make(map[int]struct{})
 	for _, num := range nums {
-		if _, found := hm[num]; found {
+		if _, found := hm[num]; foun {
 			return true
 		}
 		hm[num] = struct{}{}
