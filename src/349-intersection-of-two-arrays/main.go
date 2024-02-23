d89370abc92ae16c71e4c98478b6889aacd24f30
Arun <arun@arun.blog>
2024-02-22 19:16:23 -0800

    Manage with a single hashmap

--- src/349-intersection-of-two-arrays/main.go
+++ src/349-intersection-of-two-arrays/main.go
@@ -3,14 +3,13 @@ package main
 func intersection(nums1 []int, nums2 []int) []int {
 	managed := []int{}
 	hm := make(map[int]bool)
-	seen := make(map[int]bool)
 	for _, num1 := range nums1 {
 		hm[num1] = true
 	}
 	for _, num2 := range nums2 {
-		if hm[num2] && !seen[num2] {
+		if hm[num2] {
 			managed = append(managed, num2)
-			seen[num2] = true
+			hm[num2] = false
 		}
 	}
 	return managed
