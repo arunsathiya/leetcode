88759c716ab5a37b186489ca75faed159920dfef
Arun <arun@arun.blog>
2024-02-23 12:26:14 -0800

    Build a string sorter

--- src/49-group-anagrams/main.go
+++ src/49-group-anagrams/main.go
@@ -1,5 +1,18 @@
 package main
 
+import "sort"
+
 func groupAnagrams(strs []string) [][]string {
+	if len(strs) == 1 {
+		return [][]string{{strs[0]}}
+	}
 	return nil
+}
+
+func sortString(s string) string {
+	r := []rune(s)
+	sort.Slice(r, func(i, j int) bool {
+		return r[i] < r[j]
+	})
+	return string(r)
 }
