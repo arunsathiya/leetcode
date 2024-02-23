fdcc1157df7e1f404c6df2eb4abe85cb53a85247
Arun <arun@arun.blog>
2024-02-23 12:47:31 -0800

    Remove sorter

--- src/49-group-anagrams/main.go
+++ src/49-group-anagrams/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "sort"
-
 func groupAnagrams(strs []string) [][]string {
 	if len(strs) == 1 {
 		return [][]string{{strs[0]}}
@@ -16,12 +14,4 @@ func groupAnagrams(strs []string) [][]string {
 		result = append(result, v)
 	}
 	return result
-}
-
-func sortString(s string) string {
-	r := []rune(s)
-	sort.Slice(r, func(i, j int) bool {
-		return r[i] < r[j]
-	})
-	return string(r)
 }
