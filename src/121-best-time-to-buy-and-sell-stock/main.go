8260a78ecac88c01387cb01deb544bc176bc2b5a
Arun <arun@arun.blog>
2024-02-22 18:40:54 -0800

    Single pass approach to find the maximum profit

--- src/121-best-time-to-buy-and-sell-stock/main.go
+++ src/121-best-time-to-buy-and-sell-stock/main.go
@@ -1,5 +1,18 @@
 package main
 
 func maxProfit(prices []int) int {
-	return 0
+	buyPrice := prices[0]
+	currentProfit := 0
+	maxProfit := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < buyPrice {
+			buyPrice = prices[i]
+		} else {
+			currentProfit = prices[i] - buyPrice
+			if currentProfit > maxProfit {
+				maxProfit = currentProfit
+			}
+		}
+	}
+	return maxProfit
 }
