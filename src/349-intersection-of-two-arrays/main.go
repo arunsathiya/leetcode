3f58b0f7a921e64b78fc34ce8d56f6503e5846b2
Arun <arun@arun.blog>
2024-02-22 19:13:05 -0800

    Some progress

--- src/349-intersection-of-two-arrays/main.go
+++ src/349-intersection-of-two-arrays/main.go
@@ -1,5 +1,15 @@
 package main
 
 func intersection(nums1 []int, nums2 []int) []int {
-	return nil
+	managed := []int{}
+	hm := make(map[int]bool)
+	for _, num1 := range nums1 {
+		hm[num1] = true
+	}
+	for _, num2 := range nums2 {
+		if _, found := hm[num2]; found {
+			managed = append(managed, num2)
+		}
+	}
+	return managed
 }
