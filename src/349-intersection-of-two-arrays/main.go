c58019d2b2f7c6b4015d117ebcfb84c4bfe242e1
Arun <arun@arun.blog>
2024-02-22 19:13:41 -0800

    Track seen elements and skip them since they are already appended

--- src/349-intersection-of-two-arrays/main.go
+++ src/349-intersection-of-two-arrays/main.go
@@ -3,12 +3,14 @@ package main
 func intersection(nums1 []int, nums2 []int) []int {
 	managed := []int{}
 	hm := make(map[int]bool)
+	seen := make(map[int]bool)
 	for _, num1 := range nums1 {
 		hm[num1] = true
 	}
 	for _, num2 := range nums2 {
-		if _, found := hm[num2]; found {
+		if hm[num2] && !seen[num2] {
 			managed = append(managed, num2)
+			seen[num2] = true
 		}
 	}
 	return managed
