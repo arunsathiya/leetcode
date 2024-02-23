e466793f14f7775859c6486ee036942e83834111
Arun <arun@arun.blog>
2024-02-23 11:16:03 -0800

    Solve with hashmap

--- src/242-valid-anagram/main.go
+++ src/242-valid-anagram/main.go
@@ -1,5 +1,28 @@
 package main
 
 func isAnagram(s string, t string) bool {
-	return false
+	hm := make(map[rune]int)
+	hmt := make(map[rune]int)
+	for _, char := range s {
+		hm[char]++
+	}
+	for _, char := range t {
+		hmt[char]++
+	}
+	if !mapsEqual(hm, hmt) {
+		return false
+	}
+	return true
+}
+
+func mapsEqual(a, b map[rune]int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for k, v := range a {
+		if bv, ok := b[k]; !ok || bv != v {
+			return false
+		}
+	}
+	return true
 }
