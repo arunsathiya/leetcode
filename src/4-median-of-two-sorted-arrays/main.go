d440f509fa473d5e4016e310fab9cae15921bb15
Arun <arun@arun.blog>
2024-02-23 18:29:10 -0800

    Solve by sorting first and then finding the median of the sorted array

--- src/4-median-of-two-sorted-arrays/main.go
+++ src/4-median-of-two-sorted-arrays/main.go
@@ -6,8 +6,11 @@ import (
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	nums1 = append(nums1, nums2...)
-	sort.Slice(nums1, func(i, j int) bool {
-		return nums1[i] < nums1[j]
-	})
-	return 0
+	sort.Ints(nums1)
+	if len(nums1)%2 != 0 {
+		return float64(nums1[len(nums1)/2])
+	}
+	lower := (len(nums1) - 1) / 2
+	upper := lower + 1
+	return (float64(nums1[lower]) + float64(nums1[upper])) / 2
 }
