443c1e0ed04b7c216dd25edbedd57a47759b4ae4
Arun <arun@arun.blog>
2024-02-23 20:04:45 -0800

    Some progress but this assumes valid substrings start at the first position

--- src/3-longest-substring-without-repeating-characters/main.go
+++ src/3-longest-substring-without-repeating-characters/main.go
@@ -1,5 +1,15 @@
 package main
 
 func lengthOfLongestSubstring(s string) int {
+	managed := ""
+	chars := [26]int{}
+	for _, char := range s {
+		managed += string(char)
+		idx := int(char - 'a')
+		chars[idx]++
+		if chars[idx] >= 2 {
+			return len(managed) - 1
+		}
+	}
 	return 0
 }
