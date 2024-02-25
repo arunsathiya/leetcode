99960f72310b34b5bd84e7e8362c5a2cd7b1686a
Arun <arun@arun.blog>
2024-02-25 09:20:33 -0800

    In-place rotation by first transposing the matrix and then reversing each row

--- src/48-rotate-image/main.go
+++ src/48-rotate-image/main.go
@@ -1,5 +1,15 @@
 package main
 
 func rotate(matrix [][]int) {
-
+	n := len(matrix)
+	for i := 0; i < n; i++ {
+		for j := i; j < n; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < (n / 2); j++ {
+			matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
+		}
+	}
 }
