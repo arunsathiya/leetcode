57d2da25332a872a06bc7b41a7c0befe980aca07
Arun <arun@arun.blog>
2024-02-25 09:14:02 -0800

    Setup test cases

--- src/48-rotate-image/main_test.go
+++ src/48-rotate-image/main_test.go
@@ -1,6 +1,34 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestMain(t *testing.T) {}
+func TestRotate(t *testing.T) {
+	tests := []struct {
+		name   string
+		matrix [][]int
+		want   [][]int
+	}{
+		{
+			name:   "Example 1",
+			matrix: [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
+			want:   [][]int{{7, 4, 1}, {8, 5, 2}, {9, 6, 3}},
+		},
+		{
+			name:   "Example 2",
+			matrix: [][]int{{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}},
+			want:   [][]int{{15, 13, 2, 5}, {14, 3, 4, 1}, {12, 6, 8, 9}, {16, 7, 10, 11}},
+		},
+	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rotate(tt.matrix)
+			if !reflect.DeepEqual(tt.matrix, tt.want) {
+				t.Errorf("rotate() got = %v, want %v", tt.matrix, tt.want)
+			}
+		})
+	}
+}
