644d9731c07e12c25c72754ab712e94243fcac9a
Arun <arun@arun.blog>
2024-02-25 09:33:10 -0800

    Another approach that's a bit more readable for the reversing each row part

--- src/48-rotate-image/main.go
+++ src/48-rotate-image/main.go
@@ -6,10 +6,8 @@ func rotate(matrix [][]int) {
 		for j := i; j < n; j++ {
 			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 		}
-	}
-	for i := 0; i < n; i++ {
-		for j := 0; j < (n / 2); j++ {
-			matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
+		for j, k := 0, len(matrix[0])-1; j < k; j, k = j+1, k-1 {
+			matrix[i][j], matrix[i][k] = matrix[i][k], matrix[i][j]
 		}
 	}
 }
