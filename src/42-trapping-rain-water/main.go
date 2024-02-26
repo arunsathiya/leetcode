a11a3cde6289055c0b4224c4a18e77bce4051052
Arun <arun@arun.blog>
2024-02-26 11:48:53 -0800

    Some progress

--- src/42-trapping-rain-water/main.go
+++ src/42-trapping-rain-water/main.go
@@ -1,5 +1,31 @@
 package main
 
 func trap(height []int) int {
-	return 0
+	leftMax, rightMax := 0, 0
+	left, right := 0, len(height)-1
+	leftTrap, rightTrap := 0, 0
+	totalTrap := 0
+	for left < right {
+		if height[left] <= height[right] {
+			if height[left] < leftMax {
+				leftTrap = leftMax - height[left]
+			}
+			totalTrap += leftTrap
+			if height[left] > leftMax {
+				leftMax = height[left]
+			}
+			left++
+		}
+		if height[right] <= height[left] {
+			if height[right] < rightMax {
+				rightTrap = rightMax - height[right]
+			}
+			totalTrap += rightTrap
+			if height[right] > rightMax {
+				rightMax = height[right]
+			}
+			right--
+		}
+	}
+	return totalTrap
 }
