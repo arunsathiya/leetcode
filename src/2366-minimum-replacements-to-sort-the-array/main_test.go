8f50f8c1e81432ff1b8b5dbf54a860396d3e05c9
Arun <arun@arun.blog>
2024-02-26 09:39:16 -0800

    Setup test cases

--- src/2366-minimum-replacements-to-sort-the-array/main_test.go
+++ src/2366-minimum-replacements-to-sort-the-array/main_test.go
@@ -2,5 +2,29 @@ package main
 
 import "testing"
 
-func TestMain(t *testing.T) {}
+func TestMinimumReplacement(t *testing.T) {
+	tests := []struct {
+		name string
+		nums []int
+		want int64
+	}{
+		{
+			name: "Example 1",
+			nums: []int{3, 9, 3},
+			want: 2,
+		},
+		{
+			name: "Example 2",
+			nums: []int{1, 2, 3, 4, 5},
+			want: 0,
+		},
+	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := minimumReplacement(tt.nums); got != tt.want {
+				t.Errorf("minimumReplacement() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
