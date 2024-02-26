26a4a006a0a91487d6bf7e91b7ec9f31fe93a0f6
Arun <arun@arun.blog>
2024-02-26 11:11:40 -0800

    Look from the end and focus on operations needed count, rather than having to split with the largest value as one of the values

--- src/2366-minimum-replacements-to-sort-the-array/main.go
+++ src/2366-minimum-replacements-to-sort-the-array/main.go
@@ -1,23 +1,16 @@
 package main
 
-import "sort"
-
 func minimumReplacement(nums []int) int64 {
 	count := 0
-	for !sort.IntsAreSorted(nums) {
-		for idx, num := range nums {
-			if num > nums[idx+1] {
-				nums = replace(nums, idx, nums[idx+1], num-nums[idx+1])
-				count++
-			}
+	for i := len(nums) - 1; i > 0; i-- {
+		current := nums[i]
+		prev := nums[i-1]
+		if prev <= current {
+			continue
 		}
+		operationsNeeded := (prev + current - 1) / current
+		count += operationsNeeded - 1
+		nums[i-1] = nums[i]
 	}
 	return int64(count)
-}
-
-func replace(slice []int, index, newValue1, newValue2 int) []int {
-	slice = append(slice[:index+1], slice[index:]...)
-	slice[index] = newValue1
-	slice[index+1] = newValue2
-	return slice
 }
