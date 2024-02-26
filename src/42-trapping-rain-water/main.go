8e575fe4eac08699f93d5c23a2b3df82b0db8186
Arun <arun@arun.blog>
2024-02-26 12:10:11 -0800

    Fixes to correctly update the leftMax and rightMax, and totalTrap calculation

--- src/42-trapping-rain-water/main.go
+++ src/42-trapping-rain-water/main.go
@@ -3,26 +3,21 @@ package main
 func trap(height []int) int {
 	leftMax, rightMax := 0, 0
 	left, right := 0, len(height)-1
-	leftTrap, rightTrap := 0, 0
 	totalTrap := 0
 	for left < right {
 		if height[left] <= height[right] {
-			if height[left] < leftMax {
-				leftTrap = leftMax - height[left]
-			}
-			totalTrap += leftTrap
-			if height[left] > leftMax {
+			if height[left] >= leftMax {
 				leftMax = height[left]
+			} else {
+				totalTrap += leftMax - height[left]
 			}
 			left++
 		}
 		if height[right] <= height[left] {
-			if height[right] < rightMax {
-				rightTrap = rightMax - height[right]
-			}
-			totalTrap += rightTrap
-			if height[right] > rightMax {
+			if height[right] >= rightMax {
 				rightMax = height[right]
+			} else {
+				totalTrap += rightMax - height[right]
 			}
 			right--
 		}
