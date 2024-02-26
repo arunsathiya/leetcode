30780c63e7a970259459f79e347a58a7483c284e
Arun <arun@arun.blog>
2024-02-25 20:08:58 -0800

    Fix either p or q nil check in isSameTree problem

--- src/100-same-tree/main.go
+++ src/100-same-tree/main.go
@@ -7,11 +7,11 @@ type TreeNode struct {
 }
 
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-	if p == nil || q == nil {
-		return true
-	}
 	if p == nil && q == nil {
 		return true
+	}
+	if p == nil || q == nil {
+		return false
 	}
 	if p.Val != q.Val {
 		return false
