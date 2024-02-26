e50e8c5b1c6a90feea5d5f2479003c5e1a0b3a02
Arun <arun@arun.blog>
2024-02-26 11:29:47 -0800

    Setup test cases

--- src/42-trapping-rain-water/main.go
+++ src/42-trapping-rain-water/main.go
@@ -1,4 +1,5 @@
 package main
 
-func main() {}
-
+func trap(height []int) int {
+	return 0
+}
--- src/42-trapping-rain-water/main_test.go
+++ src/42-trapping-rain-water/main_test.go
@@ -1,6 +1,30 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestMain(t *testing.T) {}
+func TestTrap(t *testing.T) {
+	tests := []struct {
+		height []int
+		want   int
+	}{
+		{
+			height: []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1},
+			want:   6,
+		},
+		{
+			height: []int{4, 2, 0, 3, 2, 5},
+			want:   9,
+		},
+	}
 
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			got := trap(tt.height)
+			if got != tt.want {
+				t.Errorf("trap(%v) = %v, want %v", tt.height, got, tt.want)
+			}
+		})
+	}
+}
