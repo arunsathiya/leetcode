9d0781b69f85ba48d542e25e57e4970b3981dc42
Arun <arun@arun.blog>
2024-02-26 10:21:30 -0800

    Some progress. Tests pass but time limit exceeds on Leetcode

--- src/2366-minimum-replacements-to-sort-the-array/main.go
+++ src/2366-minimum-replacements-to-sort-the-array/main.go
@@ -1,5 +1,23 @@
 package main
 
+import "sort"
+
 func minimumReplacement(nums []int) int64 {
-	return 0
+	count := 0
+	for !sort.IntsAreSorted(nums) {
+		for idx, num := range nums {
+			if num > nums[idx+1] {
+				nums = replace(nums, idx, nums[idx-1], num-nums[idx-1])
+				count++
+			}
+		}
+	}
+	return int64(count)
+}
+
+func replace(slice []int, index, newValue1, newValue2 int) []int {
+	slice = append(slice[:index+1], slice[index:]...)
+	slice[index] = newValue1
+	slice[index+1] = newValue2
+	return slice
 }
