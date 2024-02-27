ee8f4df63b706875f09aeacbc038e36bc94f1c81
Arun <arun@arun.blog>
2024-02-26 18:53:07 -0800

    Some progress but I am clearly headed in the wrong direction

--- src/1291-sequential-digits/main.go
+++ src/1291-sequential-digits/main.go
@@ -1,5 +1,11 @@
 package main
 
 func sequentialDigits(low int, high int) []int {
-	return nil
+	results := make([]int, 0)
+	length := len(string(low))
+	for i := 0; i < length; i++ {
+		number := 0
+		first := 1
+	}
+	return results
 }
