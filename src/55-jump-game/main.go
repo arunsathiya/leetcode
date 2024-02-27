32afae6927a2f6d0becfcbc3eef3348396690417
Arun <arun@arun.blog>
2024-02-26 20:03:42 -0800

    Pretty much the same logic, but with an additional check that the index we are on is reachable from previously furthest reachable window

--- src/55-jump-game/main.go
+++ src/55-jump-game/main.go
@@ -1,9 +1,14 @@
 package main
 
 func canJump(nums []int) bool {
+	furthestReachable := 0
 	lastIndex := len(nums) - 1
 	for idx, num := range nums {
-		if idx+num >= lastIndex {
+		if idx > furthestReachable {
+			return false
+		}
+		furthestReachable = max(furthestReachable, idx+num)
+		if furthestReachable >= lastIndex {
 			return true
 		}
 	}
