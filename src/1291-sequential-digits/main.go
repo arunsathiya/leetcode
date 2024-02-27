6a2fc00724dc806b5c0b6b2ec37499e396455515
Arun <arun@arun.blog>
2024-02-26 18:56:39 -0800

    Retrieve substrings from the base string of specific lengths and append to results

--- src/1291-sequential-digits/main.go
+++ src/1291-sequential-digits/main.go
@@ -1,11 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func sequentialDigits(low int, high int) []int {
 	results := make([]int, 0)
-	length := len(string(low))
-	for i := 0; i < length; i++ {
-		number := 0
-		first := 1
+	base := "123456789"
+	lowl, highl := len(fmt.Sprintf("%d", low)), len(fmt.Sprintf("%d", high))
+	for length := lowl; length <= highl; length++ {
+		for start := 0; start <= 9-length; start++ {
+			substring := base[start : start+length]
+			num, _ := strconv.Atoi(substring)
+			results = append(results, num)
+		}
 	}
 	return results
 }
