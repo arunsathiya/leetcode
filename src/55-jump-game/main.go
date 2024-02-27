a0eaa0d78b63543d41aaf018236845bb5b9afa85
Arun <arun@arun.blog>
2024-02-26 19:39:09 -0800

    See if one can reach the last index in a single leap or advance to the next element

--- src/55-jump-game/main.go
+++ src/55-jump-game/main.go
@@ -1,5 +1,13 @@
 package main
 
 func canJump(nums []int) bool {
+	lastIndex := len(nums) - 1
+	for idx, num := range nums {
+		if idx+num >= lastIndex {
+			return true
+		} else {
+			continue
+		}
+	}
 	return false
 }
