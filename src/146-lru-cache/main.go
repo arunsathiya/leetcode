ed5550aa4a81e822cbbc95f1e34ddc3b6b6ec9c4
Arun <arun@arun.blog>
2024-02-26 17:00:54 -0800

    Design LRU cache

--- src/146-lru-cache/main.go
+++ src/146-lru-cache/main.go
@@ -1,4 +1,43 @@
 package main
 
-func main() {}
+import "container/list"
 
+type LRUCache struct {
+	capacity int
+	cache    map[int]*list.Element
+	list     *list.List
+}
+
+type pair struct {
+	key, value int
+}
+
+func Constructor(capacity int) LRUCache {
+	return LRUCache{
+		capacity: capacity,
+		cache:    make(map[int]*list.Element),
+		list:     list.New(),
+	}
+}
+
+func (this *LRUCache) Get(key int) int {
+	if element, found := this.cache[key]; found {
+		this.list.MoveToFront(element)
+		return element.Value.(pair).value
+	}
+	return -1
+}
+
+func (this *LRUCache) Put(key int, value int) {
+	if element, found := this.cache[key]; found {
+		this.list.MoveToFront(element)
+		element.Value = pair{key, value}
+	} else {
+		if this.list.Len() == this.capacity {
+			oldest := this.list.Back()
+			delete(this.cache, oldest.Value.(pair).key)
+			this.list.Remove(oldest)
+		}
+		this.cache[key] = this.list.PushFront(pair{key, value})
+	}
+}
