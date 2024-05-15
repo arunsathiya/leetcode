6367d4ccec0363ec4de497b005875462ade2f3ec
Arun <arun@arun.blog>
2024-05-15 05:13:26 -0700

    Some progress but it doesn't satisfy the problem requirements

--- src/443-string-compression/main.go
+++ src/443-string-compression/main.go
@@ -1,5 +1,25 @@
 package main
 
+import "strconv"
+
 func compress(chars []byte) int {
-	return 0
+	var result int
+	hm := make(map[byte]int)
+	for _, char := range chars {
+		hm[char]++
+	}
+	for _, v := range hm {
+		if v == 1 {
+			result++
+		}
+		if v > 1 && v < 10 {
+			result += 1
+			result += v
+		}
+		if v > 10 {
+			result += 1
+			result += len(strconv.Itoa(v))
+		}
+	}
+	return result
 }
