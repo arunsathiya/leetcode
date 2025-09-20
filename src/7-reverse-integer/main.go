d21abfbbd145d535e5868bca4bda12d0915f2282
Arun <arun@arun.blog>
2025-09-20 22:58:49 +0530

    fix: third test case, and add fourth test that works with 64-bit integers

--- src/7-reverse-integer/main.go
+++ src/7-reverse-integer/main.go
@@ -1,6 +1,23 @@
 package main
 
 func reverse(x int) int {
-	// TODO: implement solution
-	panic("not implemented")
+	var remainder int
+	var remaining int
+	var result int
+	originalX := x
+	if x < 0 {
+		x = -x
+	}
+	remainder = x % 10
+	remaining = x / 10
+	result += (result * 10) + int(remainder)
+	for remaining > 0 {
+		remainder = remaining % 10
+		remaining = remaining / 10
+		result = (result * 10) + remainder
+	}
+	if originalX < 0 {
+		return -result
+	}
+	return result
 }
--- src/7-reverse-integer/main_test.go
+++ src/7-reverse-integer/main_test.go
@@ -4,30 +4,45 @@ import (
 	"testing"
 )
 
-func Testreverse(t *testing.T) {
+func TestReverse(t *testing.T) {
 	tests := []struct {
-		name string
-		// TODO: add test fields
-		want interface{}
+		name     string
+		input    int
+		expected int
 	}{
 		{
-			name: "Example 1",
-			// TODO: add test case
+			name:     "first test case from Leetcode",
+			input:    123,
+			expected: 321,
+		},
+		{
+			name:     "second test case from leetcode",
+			input:    -123,
+			expected: -321,
+		},
+		{
+			name:     "third test case from leetcode",
+			input:    120,
+			expected: 21,
+		},
+		{
+			name:     "fourth test case from leetcode",
+			input:    1534236469,
+			expected: 0,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// TODO: implement test
-			// got := reverse(...)
-			// if got != tt.want {
-			//     t.Errorf("reverse() = %v, want %v", got, tt.want)
-			// }
+			output := reverse(tt.input)
+			if output != tt.expected {
+				t.Errorf("Expected %d, got %d", tt.expected, output)
+			}
 		})
 	}
 }
 
-func Benchmarkreverse(b *testing.B) {
+func BenchmarkReverse(b *testing.B) {
 	// TODO: add benchmark inputs
 	for i := 0; i < b.N; i++ {
 		// reverse(...)
