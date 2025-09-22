f0330202cae9dc86399b992323ea20eff9541c7b
Arun <arun@arun.blog>
2025-09-22 09:01:05 +0530

    fix: check for result within the 32-bit integer constraints

--- src/7-reverse-integer/main.go
+++ src/7-reverse-integer/main.go
@@ -4,6 +4,8 @@ func reverse(x int) int {
 	var remainder int
 	var remaining int
 	var result int
+	const MaxInt32 = 2147483647
+	const MinInt32 = -2147483648
 	originalX := x
 	if x < 0 {
 		x = -x
@@ -14,6 +16,12 @@ func reverse(x int) int {
 	for remaining > 0 {
 		remainder = remaining % 10
 		remaining = remaining / 10
+		if result > (MaxInt32-remainder)/10 {
+			return 0
+		}
+		if result < (MinInt32-remainder)/10 {
+			return 0
+		}
 		result = (result * 10) + remainder
 	}
 	if originalX < 0 {
